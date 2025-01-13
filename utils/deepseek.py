14a49230f59507387608efc6a57adc68a864bad1
hch <2762312941@qq.com>
2025-01-13 10:04:03 +0800

    analyse

--- utils/deepseek.py
+++ utils/deepseek.py
@@ -5,6 +5,8 @@ import argparse
 import os
 from tqdm import tqdm
 
+api_key = "sk-a81461b386a94a2cbe2c040f99cac1f3"
+
 def save_json(data, output_path):
 
     json_str = json.dumps(data, ensure_ascii=False, indent=4)
@@ -12,7 +14,6 @@ def save_json(data, output_path):
         f.write(json_str)
 
 def analyse_scope_of_knowledge(file_path):
-    api_key = "sk-a81461b386a94a2cbe2c040f99cac1f3"
 
     client = OpenAI(api_key=api_key, base_url="https://api.deepseek.com")
 
@@ -37,8 +38,51 @@ def analyse_scope_of_knowledge(file_path):
         response = client.chat.completions.create(
             model="deepseek-chat",
             messages=[
-                {"role": "system", "content": "你是一位电动力学领域的专家, 下面我会给出一个电动力学习题，请你分析一下这道题所用到的数理知识有哪些, 简短一些"},
+                {"role": "system", "content": "你是一位电动力学领域的专家, 下面我会给出一个电动力学习题，请你分析一下这道题所用到的数理知识有哪些, 简短一些, 尽量不要超过2048个token"},
                 {"role": "user", "content": f"{obj["input"]}"},
+            ],
+            stream=False
+        )
+
+        f.write(f"response: {response.choices[0].message.content}\n")
+        f.flush()
+        analyse["output"] = response.choices[0].message.content
+
+        analyses.append(analyse)
+
+        if idx%5 == 0:
+            save_json(analyses, output_path)
+        
+
+    save_json(analyses, output_path)
+
+def analyse_step_by_step(file_path):
+
+    client = OpenAI(api_key=api_key, base_url="https://api.deepseek.com")
+
+        
+    data = json.load(open(file_path, "r"))
+    output_path = os.path.join("analysis", "sbs_" + file_path.split('/')[-1]) # scope of knowlege
+    analyses = []
+    instruction = "你是一位电动力学领域的专家, 下面我会给出一个电动力学习题以及答案, 请你帮我讲解一下思路,只需要说明每一步是如何想到的,与之前步骤以及题目的关联，如果之间有比较复杂的计算证明，请尽量详细的给出计算步骤."
+
+    print("begin to ask deepseek")
+    print(f"file_path: {file_path}")
+    print(f"output_path: {output_path}")
+    f = open("analysis/analyse.txt", "w", encoding="utf-8")
+
+    for idx, obj in enumerate(tqdm(data, desc="Processing questions")):
+        question = obj["input"]
+        analyse = {}
+        analyse["instruction"] = instruction
+        analyse["input"] = f"question: {obj["input"]}\n\n answer: {obj["output"]}"
+
+        f.write(f"question {question}\n")
+        response = client.chat.completions.create(
+            model="deepseek-chat",
+            messages=[
+                {"role": "system", "content": f"{instruction} \n 尽量简短一些, 4096个token"},
+                {"role": "user", "content": f"{analyse["input"]}"},
             ],
             stream=False
         )
@@ -58,7 +102,19 @@ def analyse_scope_of_knowledge(file_path):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process a JSON file with electrodynamics problems.")
     parser.add_argument("--file", type=str, help="Path to the JSON file containing the electrodynamics problems.")
+    parser.add_argument("--analy", type=str)
+
 
     # 解析命令行参数
     args = parser.parse_args()
-    analyse_scope_of_knowledge(args.file)
+
+    analy = args.analy
+    if analy == "sok": # scope of knowledge
+        print("analyse scope of knowlege")
+        analyse_scope_of_knowledge(args.file)
+    elif analy == "sbs": # step by step
+        print("analyse step by step")
+        analyse_step_by_step(args.file)
+    else:
+        print("unknown analayse")
+        
