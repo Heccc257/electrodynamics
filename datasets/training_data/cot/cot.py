1cfd6a0f9fb9009a5d0e95b20116defe2cf07dd3
hch <2762312941@qq.com>
2025-01-29 23:07:04 +0800

    data

--- datasets/training_data/cot/cot.py
+++ datasets/training_data/cot/cot.py
@@ -18,7 +18,7 @@ file = args.file
 
 datasets = json.load(open(file, "r"))
 
-prompt = "下面将给出一个电动力学问题，请一步一步思考，使用中文给出思考过程和解答。\n    要求解答要尽量简洁,思考过程用<think></think>包裹，的每个步骤用<|>分割；答案用<answer></answer>包裹\n    注意公式使用$$并且关键字前使用\\,比如$\\sum_i$, $\\frac a b $ \n    例如：```\n    User: \"在相对论粒子的辐射中，电场 $\\boldsymbol{E}$ 的表达式是如何与加速度的平行和垂直分量相关的？\"\n    Assistant: \"<think><|>在相对论粒子的辐射中，电场 $\\boldsymbol{E}$ 的表达式可以通过粒子的加速度 $\\boldsymbol{a}$ 的平行和垂直分量来描述。<|>首先，粒子的加速度 $\\boldsymbol{a}$ 可以分解为平行于速度 $\\boldsymbol{v}$ 的分量 $\\boldsymbol{a}_\\parallel$ 和垂直于速度 $\\boldsymbol{v}$ 的分量 $\\boldsymbol{a}_\\perp$。<|>...</think>\\n <answer>线性加速器中，粒子的速度 $\\mathbf{v}$ 与加速度 $\\mathbf{a}$ 方向相同，即 $\\mathbf{v} \\parallel \\mathbf{a}$；回旋加速器中，粒子的速度 $\\mathbf{v}$ 与加速度 $\\mathbf{a}$ 方向垂直，即 $\\mathbf{v} \\perp \\mathbf{a}$。</answer>\"\n    ```\n    下面是问题：\n\n"
+prompt = "下面将给出一个电动力学问题，请一步一步思考，使用中文给出思考过程和解答。\n    要求思考过程详细，解答要尽量简洁,思考过程用<think></think>包裹,解答用<answer></answer>包裹\n    注意公式使用$$\n    例如：```\n    User: \"在相对论粒子的辐射中，电场 $\\boldsymbol{E}$ 的表达式是如何与加速度的平行和垂直分量相关的？\"\n    Assistant: \"<think>在相对论粒子的辐射中，电场 $\\boldsymbol{E}$ 的表达式可以通过粒子的加速度 $\\boldsymbol{a}$ 的平行和垂直分量来描述。...</think>\\n <answer>线性加速器中，粒子的速度 $\\mathbf{v}$ 与加速度 ...</answer>\"\n    ```\n    下面是问题：\n\n"
 
 output_dir = "datasets/training_data/cot"
 output_path = os.path.join(output_dir, file.split('/')[-1])
@@ -30,7 +30,7 @@ for data in datasets:
     outputs.append({
         "instruction": prompt,
         "input": data["instruction"],
-        "output": f"<think>{data["cot"]}</think>\n<answer>{data["output"]}</answer>",
+        "output": data["output"]
     })
 
 save_json(outputs, output_path)
